95e43263c05ba0fb870bfe34ab3fadf1279fbab7
Doodoe21 <amabdallah004@st.ug.edu.gh>
2024-05-27 16:49:51 +0000

    uoo

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is <Text styles={styles.text}>Abdul Malik Abdallah</Text></Text>
+      <Text styles={styles.text}>My name is </Text>
+        <Text styles={styles.subtitle} >Abdul Malik Abdallah</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,9 +18,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  main:{
+    flex:1,
+    justifyContent:"center",
+    maxWidth:960,
+    marginHorizontal:"auto"
+  
+  },
   text:{
+    fontSize:24,
+  },
+  subtitle:{
+    fontSize:24,
     fontWeight:"bold",
-    fontSize:24
-
-  }
+  },
 });
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # ID-11259956
-I used the command "npx create expo-app --template blank " to create a blank template
+I used the command "npx create expo-app --template blank " to create a blank template.
+I added my name to the text file "my name is Abdul Malik Abdallah"
+I changed the fontweight to bold and also changed the font size to 24
+I changed the backgrounf colour to yellow.
 ![screenshot of project](<Screenshot 2024-05-27 115729.png>)
